d91e3201a5bf06a50b2c734616002b85f7cc296c
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-30 19:26:05 +0900

    lean jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React, { Component } from 'react';
 
-function App() {
-  return <div><h1>Hello, world!</h1></div>
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" placeholder="foo" onClick={() => {console.log("I am clicked")}} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
