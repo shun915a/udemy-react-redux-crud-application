46f6c080cf1cfd38b7a71476f3176d38a655a9f0
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-04 11:51:28 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export NCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: 'INCREMENT'  
+})
+
+export const decrement = () => ({
+  type: 'DECREMENT'
+})
