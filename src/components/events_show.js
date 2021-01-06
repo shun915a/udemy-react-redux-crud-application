a6b5872ac950e4971f3c55bd92e18c4e2a1de0cf
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-06 11:13:50 +0900

    apply material-ui to events_new events_show components

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 import { getEvent, deleteEvent, putEvent } from '../actions';
 
@@ -21,10 +23,14 @@ class EventsShow extends Component {
     const { input, label, type, meta: { touched, error } } = field
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   } 
 
@@ -42,8 +48,10 @@ class EventsShow extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12 }
 
     return (
+      
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
           <Field label="Title" name="title" type="text" component={this.renderField} />
@@ -53,12 +61,10 @@ class EventsShow extends Component {
           <Field label="Body" name="body" type="text" component={this.renderField} />
         </div>
 
-        <div>
-          {/* validation check */}
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-          <Link to="/" >Cancel</Link>
-          <Link to="/" onClick={this.onDeleteClick} >Delete</Link>
-        </div>
+        <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+        <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />} />
+        <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick} />
+
       </form>
     )
   }
